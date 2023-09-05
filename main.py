0880c72295197e9745bafd4d413a1e6acf283c10
Suyamoon Pathak <partha.pathak23@gmail.com>
2023-09-05 19:59:45 +0530

    cors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route('/')
 def index():
